d995fc566b253ae1b13fdced2a46140d6fa6b3cd
Alex Halverson <alexhalverson@posteo.net>
2022-02-15 21:11:40 +1100

    improve shape

--- src/inspirations/tyler_hobbs.rs
+++ src/inspirations/tyler_hobbs.rs
@@ -1,30 +1,32 @@
 use itertools::Itertools;
 use nannou::{
-    noise::{Perlin, Seedable},
+    noise::{NoiseFn, Perlin, Seedable},
     prelude::*,
+    rand::{RngCore, SeedableRng},
 };
+use rand_chacha::ChaCha8Rng;
 
 use crate::tools::flow_field::Particle;
 
 pub struct Model {
-    particles: Option<Vec<Vec<Vec<Particle>>>>,
+    particles: Option<Vec<Vec<(Particle, Particle)>>>,
     steps: u32,
     steps_taken: u32,
     window: WindowId,
-    captured: bool
+    captured: bool,
 }
 
-const SEED: u32 = 83;
-const NUM_GROUPS: u32 = 3;
-const NUM_PARTICLES: u32 = 20;
+const SEED: u32 = 23456;
+const NUM_GROUPS: u32 = 2;
+const NUM_PAIRS: u32 = 10;
 const COLORS: [f32; 3] = [0.0, 150.0, 210.0];
 
 pub fn model(app: &App) -> Model {
-    let window = app.new_window().size(1920, 1080).fullscreen().view(view).build().unwrap();
+    let window = app.new_window().size(846, 1080).view(view).build().unwrap();
 
     Model {
         particles: None,
-        steps: 100,
+        steps: 200,
         steps_taken: 0,
         window,
         captured: false,
@@ -37,16 +39,13 @@ pub fn update(app: &App, model: &mut Model, _update: Update) {
         let direction = vec2(0.0, 10.0);
         if model.steps_taken <= model.steps {
             for p in particles {
-                for p in p {
-                    for p in p {
-                        p.move_particle_with_direction(direction, force);
-                    }
-                    // p.into_iter().for_each(|p| p.move_particle_with_direction(direction, force));
-                    // p.move_particle();
+                for (p1, p2) in p {
+                    p1.move_particle_with_direction(direction, force);
+                    p2.move_particle_with_direction(direction, force);
                 }
             }
             model.steps_taken += 1;
-        } else if model.captured == false{
+        } else if model.captured == false {
             let image_name = format!("inspirations-th {} {}", SEED, chrono::Local::now());
             app.window(model.window).unwrap().capture_frame(
                 app.project_path()
@@ -62,55 +61,61 @@ pub fn update(app: &App, model: &mut Model, _update: Update) {
 
         let half_width = window.w() / 2.0;
 
-        let particles: Vec<Vec<Vec<Particle>>> = (0..NUM_GROUPS)
+        let particles = (0..NUM_GROUPS)
             .map(|g_i| {
-                (0..NUM_PARTICLES)
+                let particle_perlin = Perlin::new().set_seed(SEED + g_i as u32);
+                println!("{}", g_i);
+                (0..NUM_PAIRS)
                     .map(|p_i| {
-                        Particle::new(
-                            map_range(
-                                random_range(-half_width, half_width),
-                                -half_width,
-                                half_width,
-                                (window.w() / NUM_PARTICLES as f32 * p_i as f32) - half_width,
-                                (window.w() / NUM_PARTICLES as f32 * p_i as f32 + 1.0) - half_width,
-                            ),
-                            (-window.h() / 2.0) + 20.0,
-                            Rect::from_wh(vec2(window.w(), window.h() - 20.0)),
-                            Box::new(Perlin::new().set_seed(SEED + g_i)),
+
+                        let mut rng = ChaCha8Rng::seed_from_u64(SEED as u64 +  g_i as u64 + p_i as u64);
+
+                        let x1 = dbg!(map_range(
+                            rng.next_u64(),
+                            u64::MIN,
+                            u64::MAX,
+                            // dbg!((window.w() / NUM_GROUPS as f32 * g_i as f32) - half_width),
+                            // dbg!((window.w() / NUM_GROUPS as f32 * (g_i as f32 + 1.0)) - half_width),
+                            dbg!((window.w() / NUM_PAIRS as f32 * p_i as f32) - half_width - 10.0),
+                            dbg!((window.w() / NUM_PAIRS as f32 * (p_i as f32 + 1.0)) - half_width + 10.0),
+
+                        ));
+                        let x2 = dbg!(map_range(
+                            rng.next_u64(),
+                            u64::MIN,
+                            u64::MAX,
+                            x1 - ( window.w() / NUM_PAIRS as f32 ),
+                            x1 + ( window.w() / NUM_PAIRS as f32 ),
+                        ));
+                        let y = (-window.h() / 2.0) + 20.0;
+
+                        let bounds = Rect::from_wh(vec2(window.w() + 50.0, window.h() - 20.0));
+                        (
+                            Particle::new(x1, y, bounds, Box::new(particle_perlin.clone()))
+                                .set_step(0.1),
+                            Particle::new(x2, y, bounds, Box::new(particle_perlin.clone()))
+                                .set_step(0.1),
                         )
-                        // .set_amplitude(6.0)
-                        // .set_frequency(0.001)
-                        .set_step(0.1)
                     })
-                    .sorted_by(|p1, p2| p1.position.x.partial_cmp(&p2.position.x).unwrap())
-                    .chunks(2)
-                    .into_iter()
-                    .map(|ps| ps.collect::<Vec<Particle>>())
-                    .collect::<Vec<Vec<Particle>>>()
+                    .collect::<Vec<(Particle, Particle)>>()
             })
             .collect();
 
-            model.particles = Some(particles);
+        model.particles = Some(particles);
     }
 }
 
 pub fn view(app: &App, model: &Model, frame: Frame) {
-    frame.clear(LIGHTGREY);
+    frame.clear(WHITE);
     let draw = app.draw();
     if let Some(particles) = &model.particles {
         for (g_i, (p, c)) in (&particles).into_iter().zip(COLORS).enumerate() {
-            for (p_i, p) in p.into_iter().enumerate() {
-                let points: Vec<Vec2> = p
-                    .iter()
-                    .map(|p| p.line().clone())
-                    .enumerate()
-                    .map(|(l_i, mut l)| {
-                        if l_i % 2 == 0 {
-                            l.reverse()
-                        }
-                        l
-                    })
-                    .flatten()
+            for (p_i, (p1, p2)) in p.into_iter().enumerate() {
+                let points: Vec<Vec2> = p1
+                    .line()
+                    .clone()
+                    .into_iter()
+                    .chain(p2.line().clone().into_iter().rev())
                     .collect();
 
                 draw.polygon().points(points).color(Hsva::new(
